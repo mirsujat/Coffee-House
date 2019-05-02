21219da8dbc796c27f37a46f47a01a49ded1e591
Mir Sujat <mir.sujat@gmail.com>
2019-05-02 20:00:40 +0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => (
