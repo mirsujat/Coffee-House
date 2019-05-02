f05670848b7772c00770e1411a6376c4a986efac
Mir Sujat <mir.sujat@gmail.com>
2019-05-02 20:53:59 +0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css"
 import "./layout.css"
+import Navbar from "./Globals/Navbar/Navbar"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,16 +19,8 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-        </div>
+        <Navbar />
+        {children}
       </>
     )}
   />
