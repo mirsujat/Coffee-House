d8165e14ba39c747fe13ec489ce0f2717523d93c
Mir Sujat <mir.sujat@gmail.com>
2019-05-03 16:22:40 +0600

    bacjground image added

---
+++ src/components/Globals/BackgroundSection/BackgroundSection.js
@@ -0,0 +1,20 @@
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+
+const BackgroundSection = ({ img, styleClass, title, children }) => {
+  return (
+    <BackgroundImage className={styleClass} fluid={img}>
+      <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold">
+        {title}
+      </h1>
+      {children}
+    </BackgroundImage>
+  )
+}
+
+BackgroundSection.defaultProps = {
+  title: "default title",
+  styleClass: "default-background",
+}
+
+export default BackgroundSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Coffee House"
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
