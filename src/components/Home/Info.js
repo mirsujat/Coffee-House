c2b176db20d6c9eef74c23e39143a16d633559b4
Mir Sujat <mir.sujat@gmail.com>
2019-05-04 11:48:30 +0600

    DOne Info component

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
+import Title from "../Globals/Title/Title";
 
 const Info = () => {
   return (
     <section className="py-5">
       <div className="container">
+        <Title title="our story" />
         <div className="row">
           <div className="col-10 col-sm-8 mx-auto text-center">
             <p className="lead text-muted mb-5">
