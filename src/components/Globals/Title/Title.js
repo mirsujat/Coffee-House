85b81ed6b6c5dc1dc4f5c0d0906833e4be14cef2
Mir Sujat <mir.sujat@gmail.com>
2019-05-04 11:48:06 +0600

    Create Title.js

---
+++ src/components/Globals/Title/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = ({ title }) => (
+  <div className="row">
+    <div className="col text-center mb-3">
+      <h1 className="display-4 text-capitalize font-weight-bold">{title}</h1>
+    </div>
+  </div>
+);
+
+export default Title;
