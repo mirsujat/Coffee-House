7f67f4f491596e6f3c2dadfd0961be264b93aae6
Mir Sujat <mir.sujat@gmail.com>
2019-05-04 21:18:09 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,9 +19,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        spaceId: "process.env.CONTENTFUL_SPACE_ID",
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+        accessToken: "process.env.CONTENTFUL_ACCESS_TOKEN"
       }
     },
     `gatsby-transformer-sharp`,
