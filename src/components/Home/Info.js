e73087a829b379c4a567c073980c15b2466f088d
Mir Sujat <mir.sujat@gmail.com>
2019-05-04 11:15:59 +0600

    Info Component added

---
+++ src/components/Home/Info.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Info = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 col-sm-8 mx-auto text-center">
+            <p className="lead text-muted mb-5">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+              quas esse quod delectus enim deleniti soluta molestias saepe. Est
+              quos beatae debitis culpa ipsum tempora et in, ab animi nesciunt.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Info;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection/BackgroundSection"
+import Info from "../components/Home/Info";
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection img={data.img.childImageSharp.fluid}  />
+    <Info></Info>
   </Layout>
 )
 
