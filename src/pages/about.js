2ef42462a0fe9c85c51250099d9502198c30bc8d
Mir Sujat <mir.sujat@gmail.com>
2019-05-04 12:33:10 +0600

    About Page added

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import BackgroundSection from "../components/Globals/BackgroundSection/BackgroundSection";
+import Info from "../components/Home/Info";
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="about us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+);
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
+export default AboutPage;
