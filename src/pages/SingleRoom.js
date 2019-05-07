ec19f128b7abd72a1a6a9e74e210773dd3d94fe9
Mir Sujat <mir.sujat@gmail.com>
2019-05-07 16:51:41 +0600

    Update SingleRoom.js

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero/Hero";
+import Banner from "../components/Banner/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 class SingleRoom extends Component {
-  state = {};
+  state = {
+    slug: this.props.match.params.slug,
+    defaultBcg
+  };
+  static contextType = RoomContext;
+
   render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    console.log(room);
+
     return (
       <div>
         <h1>Hello From Single Room</h1>
+        <Hero />
+        <Banner />
       </div>
     );
   }
