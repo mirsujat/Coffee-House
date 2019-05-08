98bb671cf78f6d539c56c306a6cc19879d04a8af
Mir Sujat <mir.sujat@gmail.com>
2019-05-08 21:22:37 +0600

    working on filter

--- src/components/RoomFilter/RoomFilter.js
+++ src/components/RoomFilter/RoomFilter.js
@@ -1,10 +1,61 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../../context";
+import Title from "../Title/Title";
 
-const RoomFilter = () => {
+// get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+const RoomFilter = ({ rooms }) => {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  // get unique types
+  let types = getUnique(rooms, "type");
+  // add all
+  types = ["all", ...types];
+  // map to JSX
+  types = types.map((item, i) => {
+    return (
+      <option key={i} value={item}>
+        {item}
+      </option>
+    );
+  });
+
   return (
-    <div>
-      <h4>Hello From RoomFilter</h4>
-    </div>
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="id"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end of select type */}
+      </form>
+    </section>
   );
 };
 
--- src/context.js
+++ src/context.js
@@ -9,19 +9,36 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   componentDidMount = () => {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   };
+
+  // format data
   formatData = items => {
     let tempItems = items.map(item => {
       let id = item.sys.id;
@@ -32,15 +49,34 @@ class RoomProvider extends Component {
 
     return tempItems;
   };
+
+  // find single room id or value
   getRoom = slug => {
     let tempRooms = [...this.state.rooms];
     const room = tempRooms.find(room => room.slug === slug);
     return room;
   };
 
+  // handle input change
+  handleChange = event => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+  filterRooms = () => {
+    console.log("Hello from filterRooms");
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
